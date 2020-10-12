a56e5219d8a278263a258bda68125ff90117cb0f
adonisShareef <adonis_shareef@hotmail.com>
2020-10-11 20:58:40 -0700

    commented my code

--- Shareef_DSC510/Shareef_6.1.py
+++ Shareef_DSC510/Shareef_6.1.py
@@ -11,23 +11,30 @@
 # Change Approved by: Adonis Shareef
 # Date Moved to Production: 10/11/2020
 
+# empty list
 temperatures = []
 while True:
     try:
+# user input which gets how many temps you want to input, must be a whole number
         numoftemps = int(input("Enter how many temperatures you wish to input "))
         break
     except ValueError:
+# the user must enter a number
         print("Enter a number")
         continue
 print("Enter your numbers and press Enter after each number ")
 for i in range(numoftemps):
-        while True:
-            try:
-                temperatures.append(float(input()))
-                break
-            except ValueError:
-                print("Enter a valid temperature ex:(90,-30,101) ")
-                continue
-print("The Largest temperature: ",max(temperatures))
-print("The Smallest temperature: ",min(temperatures))
-print("Number of temperatures entered: ",len(temperatures))
+    while True:
+        try:
+# adds the user input to the list
+            temperatures.append(float(input()))
+            break
+        except ValueError:
+            print("Enter a valid temperature ex:(90,-30,101) ")
+            continue
+# using max function on the list to return the largest element
+print("The Largest temperature: ", max(temperatures))
+# using the min function on the list to return the smallest element
+print("The Smallest temperature: ", min(temperatures))
+# returning the length of the list
+print("Number of temperatures entered: ", len(temperatures))
